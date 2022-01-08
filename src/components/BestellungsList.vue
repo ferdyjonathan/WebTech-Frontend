c9ee7e9302a4938ad96cca682bf4440ce52db673
ferdyjonathan <ferdyjonathan@yahoo.com>
2022-01-09 00:25:27 +0100

    Adding some Search Function and sorting function

--- src/components/BestellungsList.vue
+++ src/components/BestellungsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bestellungslist"></div>
   <div id="all-products">
-  <table class="table table-hover" style=" overflow : hidden; border-radius : 4px ; box-shadow: 0 4px 20px rgba(0, 0, 0, 0, 15);">
+  <table class= "table table-hover" style=" overflow : hidden; border-radius : 4px ; box-shadow: 0 4px 20px rgba(0, 0, 0, 0, 15);">
     <thead>
     <tr style="color : white; font-weight : bold; background: #3a6d8c ">
       <td>Id</td>
@@ -10,18 +10,19 @@
       <td>Payment</td>
       <td>Status</td>
       <td>Total price</td>
+      <td></td>
     </tr>
     </thead>
     <tbody>
-    <tr v-for="bestellung in bestellungs" :key="bestellung.id">
+    <tr v-for="(bestellung, index) in sortedArray" v-bind:key="bestellung.id">
       <td>{{ bestellung.id }}</td>
       <td>{{ bestellung.name }}</td>
       <td>{{ bestellung.paket }}</td>
       <td>{{ bestellung.payment? 'Bezahlt' : 'Unbezahlt' }}</td>
       <td>{{ bestellung.status }}</td>
       <td>{{ bestellung.totalprice }}</td>
-      <!--  <router-link :to="{name: 'edit_product', params: { id: bestellung.id }}" class="btn btn-outline-primary" style="">Modifier</router-link>-->
-      <!--   <router-link :to="{name: 'delete-product', params: { id: bestellung.id }}" class="btn btn-outline-danger">Supprimer</router-link>-->
+      <td><router-link :to="{ name: 'edit_product', params: { id: bestellung.id }}" class="btn btn-danger">Edit</router-link>
+        <button class="btn btn-danger" v-on:click="deleteOrder(index)">Delete</button></td>
     </tr>
     </tbody>
   </table>
@@ -31,10 +32,29 @@
 <script>
 export default {
   name: 'BestellungsList',
+  components: {},
+  computed: {
+    sortedArray: function () {
+      function compare (a, b) {
+        if (a.id < b.id) { return -1 }
+        if (a.id > b.id) { return 1 }
+        return 0
+      }
+
+      // eslint-disable-next-line vue/no-mutating-props,vue/no-side-effects-in-computed-properties
+      return this.bestellungs.sort(compare)
+    }
+  },
   props: {
     bestellungs: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    deleteOrder (id) {
+      // eslint-disable-next-line vue/no-mutating-props
+      this.bestellungs.splice(id, 1)
     }
   }
 }
